47de0a7a928db5e5db5f7fd3c04c2f50a3007216
Katherine Bellman <katherine.bellman@dcmail.ca>
2022-06-22 22:54:15 -0400

    added favicon

---
+++ src/components/MainLayout/components/icon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ReactComponent as BrandIcon } from "./icon.svg";
+
+const MyIcon = () => {
+    return(
+        <BrandIcon />
+    );
+}
+
+export default MyIcon ;
